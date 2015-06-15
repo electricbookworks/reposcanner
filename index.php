d20b762e7bf3094d5afc1193ce52d7b388fb038d
Arthur Attwell <arthur@arthurattwell.com>
2015-06-15 17:28:55 +0200

    Add archive pagination

--- index.php
+++ index.php
@@ -12,6 +12,25 @@ get_sidebar();
 <?php
 get_template_part( 'loop' ); 
 ?>
+
+<div class="pagination">
+<?php
+/**
+ * Paginate: https://codex.wordpress.org/Function_Reference/paginate_links
+ */
+global $wp_query;
+
+$big = 999999999; // need an unlikely integer
+
+echo paginate_links( array(
+	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format' => '?paged=%#%',
+	'current' => max( 1, get_query_var('paged') ),
+	'total' => $wp_query->max_num_pages
+) );
+?>
+</div><!--.pagination-->
+
 </div><!--#archive-->
 <?php
 get_footer();
